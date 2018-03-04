a70f619fe74d3cd5f6f9ed9cb80fe9ea972717ff
ganya7 <30626883+ganya7@users.noreply.github.com>
2018-03-04 23:53:31 +0530

    feature selection method

--- object_detection/histogram_of_oriented_gradients.py
+++ object_detection/histogram_of_oriented_gradients.py
@@ -1,13 +1,7 @@
-
-# coding: utf-8
-
 # ## Histogram of Oriented Gradients
-# 
+#
 # - http://stackoverflow.com/questions/6090399/get-hog-image-features-from-opencv-python
 # - http://www.juergenwiki.de/work/wiki/doku.php?id=public:hog_descriptor_computation_and_visualization
-
-# In[2]:
-
 
 import numpy as np
 import cv2
@@ -22,10 +16,10 @@ cv2.imshow('Input Image', image)
 cv2.waitKey(0)
 
 # h x w in pixels
-cell_size = (8, 8) 
+cell_size = (8, 8)
 
- # h x w in cells
-block_size = (2, 2) 
+# h x w in cells
+block_size = (2, 2)
 
 # number of orientation bins
 nbins = 9
@@ -47,23 +41,23 @@ n_cells = (gray.shape[0] // cell_size[0], gray.shape[1] // cell_size[1])
 # hog_feats now contains the gradient amplitudes for each direction,
 # for each cell of its group for each group. Indexing is by rows then columns.
 hog_feats = hog.compute(gray).reshape(n_cells[1] - block_size[1] + 1,
-                        n_cells[0] - block_size[0] + 1,
-                        block_size[0], block_size[1], nbins).transpose((1, 0, 2, 3, 4))  
+                                      n_cells[0] - block_size[0] + 1,
+                                      block_size[0], block_size[1], nbins).transpose((1, 0, 2, 3, 4))
 
-# Create our gradients array with nbin dimensions to store gradient orientations 
+# Create our gradients array with nbin dimensions to store gradient orientations
 gradients = np.zeros((n_cells[0], n_cells[1], nbins))
 
-# Create array of dimensions 
+# Create array of dimensions
 cell_count = np.full((n_cells[0], n_cells[1], 1), 0, dtype=int)
 
 # Block Normalization
 for off_y in range(block_size[0]):
     for off_x in range(block_size[1]):
         gradients[off_y:n_cells[0] - block_size[0] + off_y + 1,
-                  off_x:n_cells[1] - block_size[1] + off_x + 1] += \
+        off_x:n_cells[1] - block_size[1] + off_x + 1] += \
             hog_feats[:, :, off_y, off_x, :]
         cell_count[off_y:n_cells[0] - block_size[0] + off_y + 1,
-                   off_x:n_cells[1] - block_size[1] + off_x + 1] += 1
+        off_x:n_cells[1] - block_size[1] + off_x + 1] += 1
 
 # Average gradients
 gradients /= cell_count
@@ -77,4 +71,3 @@ plt.gca().set_aspect('equal', adjustable='box')
 plt.colorbar()
 plt.show()
 cv2.destroyAllWindows()
-
